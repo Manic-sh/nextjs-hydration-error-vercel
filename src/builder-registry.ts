3347c8d3b69e783716b9bb9ae326e2db2b7fa0a9
Manic-sh <mash.pro666@gmail.com>
2024-07-24 16:11:10 +0530

    Testing showif

--- src/builder-registry.ts
+++ src/builder-registry.ts
@@ -86,10 +86,32 @@ Builder.registerComponent(HeaderNavigation, {
           defaultValue: "/",
         },
       ],
+      showIf: (options) => {
+        return options.get('label') 
+      }
     },
     {
       name: "logo",
       type: "file",
     }
   ],
+});
+
+Builder.register("editor.settings", {
+  styleStrictMode: true, // optional
+  designTokens: {
+    colors: [
+      { name: "Brand Red", value: "var(--red, #ff0000)" },
+      { name: "Brand Blue", value: "rgba(93, 150, 255, 1)" },
+    ],
+    spacing: [
+      { name: "Large", value: "var(--space-large, 20px)" },
+      { name: "Small", value: "var(--space-small, 10px)" },
+      { name: "Tiny", value: "5px" },
+    ],
+    fontFamily: [
+      { name: 'Serif Font', value: 'var(--serif-font, Times, serif)' },
+      { name: 'Primary Font', value: 'Roboto, sans-serif' },
+    ]
+  },
 });
